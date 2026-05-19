4e6bd9d29ff62f141b4b4ecb8f291afadd3fed7b
Shashi <Shashi@gmail.com>
2026-05-19 11:40:48 +0530

    removed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -23,9 +23,6 @@ const App = () => {
     <div className="bg-[#F8F9FD]">
       <ToastContainer />
       <Navbar />
-      <h1 className="text-5xl text-red-600 font-bold text-center mt-5">
-  TEST DEPLOY
-</h1>
       <div className="flex items-start">
         <Sidebar />
         <Routes>
